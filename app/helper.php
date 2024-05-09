cb3d89429f3fd98e0f935e280516d48553cfdf08
aayush <aayush@softsaro.com>
2024-05-09 12:20:38 +0545

    main url aadded

--- app/helper.php
+++ app/helper.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
-
 function baseUrl()
 {
-    return "http://127.0.0.1:8000/";
+   return config('app.main_url');
 }
+
 function getCategories($parent_id)
 {
     return DB::table('categories')->where('parent_id', $parent_id)->orderBy('category_order', 'ASC')->limit(14)->get();
