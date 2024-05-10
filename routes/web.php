5e1da9cdcb24939117072b2ace286c6c4b716972
anup354 <anupkasula5@gmail.com>
2024-05-10 21:44:21 +0545

    changes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,25 @@ use App\Http\Controllers\Seller\ProductController;
 use App\Http\Controllers\Seller\SellerAuthController;
 use App\Http\Controllers\Seller\SellerDashboardController;
 use App\Http\Controllers\Seller\SellerProfileController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Welcome to my Laravel application!';
+});
+
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    return 'Migration has been successfully';
+});
+
+Route::get('/clear', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:cache');
+    Artisan::call('config:cache');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    return 'Application all kind of cache has been cleared';
 });
 Route::controller(SellerAuthController::class)->group(function () {
     Route::get('/login',  'login')->name('login');
