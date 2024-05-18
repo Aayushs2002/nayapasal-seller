50f4963c9dd641a39b88756923c01969ce742974
aayush <aayush@softsaro.com>
2024-05-18 19:03:23 +0545

    order in seller

---
+++ app/Http/Controllers/Seller/SellerOrderController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderBillingInfo;
+use App\Services\Seller\Order\OrderService;
+use Illuminate\Http\Request;
+
+class SellerOrderController extends Controller
+{
+    public function __construct(
+        protected OrderService $orderService
+    ) {     
+    }
+
+    public function order(){
+        $order_list = $this->orderService->order();
+        // dd($order_list);
+        return view('Seller.order.order', compact('order_list'));
+   
+    }
+
+    public function orderdetail($order_id){
+        // dd($order_id);
+
+        $orderdetail = $this->orderService->orderdetails($order_id);         
+        // dd($orderdetail);
+        $order = $orderdetail[0];
+        $productdetails = $orderdetail[1];
+        $user_details = $orderdetail[2];
+        return view('Seller.order.order_detail',compact('order','productdetails','user_details'));
+        
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'order_id',
+        'order_status',
+        'payment_method',
+        'amount',
+        'items',
+        'payment_status',
+        'view_status',
+        'user_id',
+        'order_from',
+        'delivery_charge',
+        'taxpercent',
+        'taxamount',
+        'coupon',
+        'delivered_date',
+    ];
+
+    public function orderItem()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id', 'id');
+    }
+}
